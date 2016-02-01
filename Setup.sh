8836a9890f27489a51108f1c3909645670c05f61
Alexander Rymdeko-Harvey <killswitch-GUI@users.noreply.github.com>
2016-02-01 16:42:27 -0500

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -40,6 +40,7 @@ func_install_requests(){
   #sudo apt-get -q upgrade 
   sudo git pull
   sudo apt-get -y install wget grep antiword odt2txt
+  pip install beautifulsoup4 --upgrade
   sudo pip install docx2txt --upgrade
   sudo pip install --upgrade requests 
   sudo pip install xlsx2csv --upgrade
