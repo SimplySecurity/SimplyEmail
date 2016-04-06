e706c1399877b8ce052de92562a4892cfff7325f
Killswitch-GUI <iamfree2009@gmail.com>
2016-04-06 18:35:37 -0400

    PEP8 Fixes

--- Common/TaskController.py
+++ Common/TaskController.py
@@ -52,19 +52,6 @@ class Conducter:
         except Exception as e:
             print e
 
-    def ConfigSectionMap(section):
-        dict1 = {}
-        options = Config.options(section)
-        for option in options:
-            try:
-                dict1[option] = Config.get(section, option)
-                if dict1[option] == -1:
-                    DebugPrint("skip: %s" % option)
-            except:
-                print("exception on %s!" % option)
-                dict1[option] = None
-        return dict1
-
     def TestModule(self, module, domain):
         ModuleName = module
         module = self.modules[module]
