147628b28d5dc44db4ae7ba37ef1c1c4e8df8b35
Killswitch-GUI <iamfree2009@gmail.com>
2016-04-06 21:49:43 -0400

    PEP8

--- Helpers/VersionCheck.py
+++ Helpers/VersionCheck.py
@@ -3,29 +3,31 @@ import requests
 import configparser
 import helpers
 
+
 class VersionCheck:
+
     def __init__(self, version):
-      config = configparser.ConfigParser()
-      try:
-        self.version = str(version)
-        config.read('Common/SimplyEmail.ini')
-        self.Start = config['GlobalSettings']['VersionRepoCheck']
-        self.RepoLocation = str(config['GlobalSettings']['VersionRepoCheckLocation'])
-        self.UserAgent = {
-          'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-      except Exception as e:
-        print e
+        config = configparser.ConfigParser()
+        try:
+            self.version = str(version)
+            config.read('Common/SimplyEmail.ini')
+            self.Start = config['GlobalSettings']['VersionRepoCheck']
+            self.RepoLocation = str(
+                config['GlobalSettings']['VersionRepoCheckLocation'])
+            self.UserAgent = {
+                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+        except Exception as e:
+            print e
 
     def VersionRequest(self):
-      if self.Start == "Yes":
-        try:
-          r = requests.get(self.RepoLocation, headers=self.UserAgent)
-          results = r.content
-          results = results.rstrip('\n')
-          if str(results) != str(self.version):
-            p = " [!] Newer Version Available, Re-Run Setup.sh to update!"
-            print helpers.color(p, warning=True,bold=False)
-        except Exception as e:
-          error = "[!] Fail during Request to Update/Version Check (Check Connection)"
-          print helpers.color(error, warning=True)
-
+        if self.Start == "Yes":
+            try:
+                r = requests.get(self.RepoLocation, headers=self.UserAgent)
+                results = r.content
+                results = results.rstrip('\n')
+                if str(results) != str(self.version):
+                    p = " [!] Newer Version Available, Re-Run Setup.sh to update!"
+                    print helpers.color(p, warning=True, bold=False)
+            except Exception as e:
+                error = "[!] Fail during Request to Update/Version Check (Check Connection)"
+                print helpers.color(error, warning=True)
