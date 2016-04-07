795840da9178987edfd1e928f826a80494a5ed38
Alexander Rymdeko-Harvey <killswitch-GUI@users.noreply.github.com>
2016-04-07 10:40:21 -0400

    Update GitHubUserSearch.py

--- Modules/GitHubUserSearch.py
+++ Modules/GitHubUserSearch.py
@@ -12,7 +12,7 @@ from Helpers import helpers
 # so lets add a 5 sec sleep per request
 
 
-class ClassName:
+class ClassName(object):
 
     def __init__(self, domain, verbose=False):
         self.apikey = False
