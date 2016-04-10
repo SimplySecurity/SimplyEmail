8af7d5dad8d25c6c99487565276d0e114fc795d7
Killswitch-GUI <iamfree2009@gmail.com>
2016-04-10 18:51:40 -0400

    nose tests

--- tests/test_simplyemail_list.py
+++ tests/test_simplyemail_list.py
@@ -5,7 +5,18 @@ from Helpers import EmailFormat
 from Helpers import HtmlBootStrapTheme
 from Helpers import Connect6
 from Helpers import Parser
+from Common import TaskController
+from Helpers import VersionCheck
+import SimplyEmail
 
 assert helpers.color('test')
 assert helpers.color('test', firewall=True)
 assert helpers.color('test', warning=True)
+
+
+# perfrom list funcs
+Task = TaskController.Conducter()
+Task.load_modules()
+Task.ListModules()
+V = VersionCheck.VersionCheck("1.3")
+V.VersionRequest()
