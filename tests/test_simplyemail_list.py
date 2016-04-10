271169f2f7d773b8d787b6d2ad13d0160154b0c3
Alexander Rymdeko-Harvey <killswitch-GUI@users.noreply.github.com>
2016-04-10 18:13:01 -0400

    Create test_simplyemail_list.py

---
+++ tests/test_simplyemail_list.py
@@ -0,0 +1,11 @@
+from Helpers import helpers
+from Helpers import downloads
+from Helpers import parser
+from Helpers import EmailFormat
+from Helpers import HtmlBootStrapTheme
+from Helpers import Connect6
+from Helpers import Parser
+
+assert print helpers.color('test')
+assert print helpers.color('test', firewall=True)
+assert print helpers.color('test', warning=True)
