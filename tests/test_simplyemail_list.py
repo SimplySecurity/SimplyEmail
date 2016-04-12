7b518947a94bc810a2cb5a2c6fae63eae5f0e117
Killswitch-GUI <iamfree2009@gmail.com>
2016-04-11 21:41:24 -0400

    Unit Testing

--- tests/test_simplyemail_list.py
+++ tests/test_simplyemail_list.py
@@ -46,24 +46,24 @@ def test_emailformat():
     finalemails = ['mmax@verisgroup.com']
     result = em.EmailDetect(cleannames, domain, finalemails)
     assert result[0] == '{f}{last}'
-    finalemails = ['m.max@verisgroup.com'
+    finalemails = ['m.max@verisgroup.com']
     result = em.EmailDetect(cleannames, domain, finalemails)
     assert result[0] == '{f}.{last}'
-    finalemails = ['madmax@verisgroup.com'
+    finalemails = ['madmax@verisgroup.com']
     result = em.EmailDetect(cleannames, domain, finalemails)
     assert result[0] == '{first}{last}'
-    finalemails = ['mad.max@verisgroup.com'
+    finalemails = ['mad.max@verisgroup.com']
     result = em.EmailDetect(cleannames, domain, finalemails)
     assert result[0] == '{first}.{last}'
-    finalemails = ['mad.m@verisgroup.com'
+    finalemails = ['mad.m@verisgroup.com']
     result = em.EmailDetect(cleannames, domain, finalemails)
     assert result[0] == '{first}.{l}'
-    finalemails = ['madm@verisgroup.com'
+    finalemails = ['madm@verisgroup.com']
     result = em.EmailDetect(cleannames, domain, finalemails)
     assert result[0] == '{first}{l}'
-    finalemails = ['mad_max@verisgroup.com'
+    finalemails = ['mad_max@verisgroup.com']
     result = em.EmailDetect(cleannames, domain, finalemails)
     assert result[0] == '{first}_{last}'
-    finalemails = ['mad@verisgroup.com'
+    finalemails = ['mad@verisgroup.com']
     result = em.EmailDetect(cleannames, domain, finalemails)
     assert result[0] == '{first}'
