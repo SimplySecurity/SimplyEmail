c42fc89ab747600a472aee492470c94be10f2cdc
Alexander Rymdeko-Harvey <killswitch-GUI@users.noreply.github.com>
2016-04-20 09:49:07 -0400

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -47,7 +47,7 @@ func_install_requests(){
   sudo pip install mechanize --upgrade
   sudo pip install docx2txt --upgrade
   sudo pip install BeautifulSoup --upgrade
-  sudo pip install --upgrade requests 
+  sudo pip install requests --upgrade
   sudo pip install xlsx2csv --upgrade
   sudo pip install configparser --upgrade
   sudo pip install BeautifulSoup --upgrade
