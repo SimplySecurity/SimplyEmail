b921c86be7b22613abb7cdf69e25e3166722064f
root <root@localhost.localdomain>
2017-11-01 10:36:01 -0400

    init

---
+++ setup/oneline-setup.sh
@@ -0,0 +1,22 @@
+func_check_env(){
+  # Check Sudo Dependency going to need that!
+  if [ $(which sudo|wc -l) -eq '0' ]; then
+    echo
+    echo ' [ERROR]: This Setup Script Requires sudo!'
+    echo '          Please Install sudo Then Run This Setup Again.'
+    echo
+    exit 1
+  fi
+  
+  git clone --branch dev https://github.com/killswitch-GUI/SimplyEmail.git
+  cd SimplyEmail
+  ./setup/setup.sh
+}
+
+
+case $1 in
+  *)
+  func_check_env
+  ;;
+
+esac
