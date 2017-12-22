86d164a18b2413b6e7d6789b4d1c403dd0393afe
⭕Alexander Rymdeko-Harvey <8761706+killswitch-GUI@users.noreply.github.com>
2017-12-22 17:00:30 -0500

    Update oneline-setup.sh

--- setup/oneline-setup.sh
+++ setup/oneline-setup.sh
@@ -1,22 +1,29 @@
 func_check_env(){
   # Check Sudo Dependency going to need that!
+  if [ -f /etc/debian_version ]; then
+    sudo apt install -y git
+    sudo apt install -y curl
+    sudo apt install -y sudo
+  fi
+  
+  if [ -f /etc/redhat-release ]; then
+    sudo dnf install -y git
+    sudo dnf install -y curl
+    sudo dnf install -y sudo
+  fi
+
+  if [ -f /etc/lsb-release ]; then
+    sudo apt-get -y install git
+    sudo apt-get -y install curl
+    sudo apt-get -y install sudo
+  fi
+  
   if [ $(which sudo|wc -l) -eq '0' ]; then
     echo
     echo ' [ERROR]: This Setup Script Requires sudo!'
     echo '          Please Install sudo Then Run This Setup Again.'
     echo
     exit 1
-  fi
-  if [ -f /etc/debian_version ]; then
-    sudo apt install -y git
-  fi
-  
-  if [ -f /etc/redhat-release ]; then
-    sudo dnf install -y git
-  fi
-
-  if [ -f /etc/lsb-release ]; then
-    sudo apt-get -y install git
   fi
   
   git clone --branch master https://github.com/killswitch-GUI/SimplyEmail.git
