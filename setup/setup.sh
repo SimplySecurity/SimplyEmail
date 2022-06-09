6db699d4d7e4f87d73e90054ffaaa44a42127be2
Aaron Will Djaba <61732071+kurtiz@users.noreply.github.com>
2022-06-09 20:31:51 +0000

    FIxed path error for colab

--- setup/setup.sh
+++ setup/setup.sh
@@ -90,7 +90,7 @@ func_install_env(){
 }
 
 func_install_pip(){
-   pip install -r /content/setup/requirments.txt 
+   pip install -r /content/SimplyEmail/setup/requirments.txt 
 }
 
 # Menu Case Statement
